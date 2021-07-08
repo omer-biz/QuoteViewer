aedaaadfb05a9a2e5b636752483f298e5d078965
Nebil <37885814+itismrx@users.noreply.github.com>
2021-07-08 21:16:18 +0300

    Update MainClass.java
    Instantiating Objects

--- MainClass.java
+++ MainClass.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class MainClass {
 
     public static void main(String arg[]) {
+        QuoteList qouteList = new ProxyQuoteList();
+        QuoteRepository qouteRepo = new QuoteRepository();
 
+        qouteRepo.setQuotes(qouteList.getQuotes());
     }
 }
