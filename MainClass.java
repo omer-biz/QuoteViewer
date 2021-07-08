bb78fe7f06e13b5596d8cffe71963253ed91cede
Surafel Hailu <surafelhd@pm.me>
2021-07-08 23:40:32 +0300

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -7,5 +7,12 @@ public class MainClass {
         QuoteRepository qouteRepo = new QuoteRepository();
 
         qouteRepo.setQuotes(qouteList.getQuotes());
+        int nexOrPrev = 1; // 1 means next, 0 means previous
+        Quote quote = null;
+        Scanner input = new Scanner(System.in);
+
+        Iterator iter = qouteRepo.getIterator();
+        quote = (Quote)iter.next();
+        System.out.println(quote);
     }
 }
